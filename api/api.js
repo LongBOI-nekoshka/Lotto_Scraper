4c201203920563f6b7786c92f61eabe4c9d97c81
Adam.Sobremonte <adam@newmediastaff.com>
2021-11-17 23:14:36 +0800

    Added many

--- api/api.js
+++ api/api.js
@@ -1,9 +1,9 @@
 const LottoController = require('../Controller/LottoController');
 
 const api = (app) => {
-    app.get('/test',async  (req,res) =>  {
-        await LottoController.scrape()
-        res.send().status(200);
+    app.get('/test', async (req,res) =>  {
+        res.send(await LottoController.scrape());
+        // res.sendStatus(200);
     })
 };
 
